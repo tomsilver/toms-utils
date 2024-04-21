0f5e85f467c121f8d0c473eceeb4919bc8c9a912
Tom Silver <tomssilver@gmail.com>
2024-04-21 16:26:48 -0400

    add angle wrap utility

--- src/tomsutils/utils.py
+++ src/tomsutils/utils.py
@@ -54,3 +54,8 @@ def fig2data(fig: plt.Figure) -> Image:
     """Convert matplotlib figure into Image."""
     fig.canvas.draw()
     return np.array(fig.canvas.renderer.buffer_rgba())
+
+
+def wrap_angle(angle: float) -> float:
+    """Wrap an angle in radians to [-pi, pi]."""
+    return np.arctan2(np.sin(angle), np.cos(angle))
