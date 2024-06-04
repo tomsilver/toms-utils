5a8a5b0fd013793eb01d4f1f92406feea84008ab
Tom Silver <tomsilver@users.noreply.github.com>
2024-06-04 12:40:38 -0400

    add pyperplan task planning utils (#14)

--- src/tomsutils/pddl_planning.py
+++ src/tomsutils/pddl_planning.py
@@ -1,10 +1,13 @@
 """Utilities for planning in PDDL problems."""
 
+import logging
 import os
 import re
 import subprocess
 import tempfile
 from typing import List, Optional
+
+from pyperplan.planner import HEURISTICS, SEARCHES, search_plan
 
 
 def run_pddl_planner(
@@ -16,7 +19,40 @@ def run_pddl_planner(
         return run_fastdownward_planning(domain_str, problem_str, alias="lama-first")
     if planner == "fd-opt":
         return run_fastdownward_planning(domain_str, problem_str, alias="seq-opt-lmcut")
+    if planner == "pyperplan":
+        return run_pyperplan_planning(domain_str, problem_str)
     raise NotImplementedError(f"Planner {planner} not implemented.")
+
+
+def run_pyperplan_planning(
+    domain_str: str,
+    problem_str: str,
+    heuristic: str = "hff",
+    search: str = "gbf",
+) -> Optional[List[str]]:
+    """Find a plan with pyperplan."""
+    search_fn = SEARCHES[search]
+    heuristic_fn = HEURISTICS[heuristic]
+    domain_file = tempfile.NamedTemporaryFile(delete=False).name
+    problem_file = tempfile.NamedTemporaryFile(delete=False).name
+    with open(domain_file, "w", encoding="utf-8") as f:
+        f.write(domain_str)
+    with open(problem_file, "w", encoding="utf-8") as f:
+        f.write(problem_str)
+    # Quiet the pyperplan logging.
+    logging.disable(logging.ERROR)
+    pyperplan_plan = search_plan(
+        domain_file,
+        problem_file,
+        search_fn,
+        heuristic_fn,
+    )
+    logging.disable(logging.NOTSET)
+    os.remove(domain_file)
+    os.remove(problem_file)
+    if pyperplan_plan is None:
+        return None
+    return [a.name for a in pyperplan_plan]
 
 
 def run_fastdownward_planning(
