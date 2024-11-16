532ae32b42e49e3eb8c813fb389cbc227ba55569
Tom Silver <tomsilver@users.noreply.github.com>
2024-11-16 10:28:01 -0500

    add FunctionalSpace (#24)

--- src/tomsutils/spaces.py
+++ src/tomsutils/spaces.py
@@ -1,6 +1,6 @@
 """Utilities for gymnasium-like spaces."""
 
-from typing import Any, Sequence, TypeVar
+from typing import Any, Callable, Sequence, TypeVar
 
 import gymnasium as gym
 import numpy as np
@@ -32,3 +32,32 @@ class EnumSpace(gym.spaces.Space[_Element]):
 
     def __repr__(self) -> str:
         return f"EnumSpace({self._elements})"
+
+
+class FunctionalSpace(gym.spaces.Space[_Element]):
+    """A space defined with explicit sampling and contains functions."""
+
+    def __init__(
+        self,
+        contains_fn: Callable[[Any], bool],
+        sample_fn: Callable[[np.random.Generator], _Element] | None = None,
+        seed: int | np.random.Generator | None = None,
+    ) -> None:
+        super().__init__(shape=None, dtype=None, seed=seed)
+        self._sample_fn = sample_fn
+        self._contains_fn = contains_fn
+
+    def sample(self, mask: Any | None = None) -> _Element:
+        if self._sample_fn is None:
+            raise NotImplementedError("Sampling not implemented for space")
+        return self._sample_fn(self.np_random)
+
+    def contains(self, x: Any) -> bool:
+        return self._contains_fn(x)
+
+    @property
+    def is_np_flattenable(self) -> bool:
+        return False
+
+    def __repr__(self) -> str:
+        return "FunctionalSpace()"
--- tests/test_spaces.py
+++ tests/test_spaces.py
@@ -1,6 +1,6 @@
 """Tests for spaces.py."""
 
-from tomsutils.spaces import EnumSpace
+from tomsutils.spaces import EnumSpace, FunctionalSpace
 
 
 def test_enum_space():
@@ -12,3 +12,20 @@ def test_enum_space():
     assert elm in elements
     assert space.contains(elm)
     assert not space.is_np_flattenable
+
+
+def test_functional_space():
+    """Tests for FunctionalSpace()."""
+
+    def _sample_fn(rng):
+        return float(rng.uniform(-3, 3))
+
+    def _contains_fn(x):
+        if not isinstance(x, float):
+            return False
+        return -3 <= x <= 3
+
+    space = FunctionalSpace(_contains_fn, _sample_fn, seed=123)
+    for _ in range(10):
+        x = space.sample()
+        assert space.contains(x)
